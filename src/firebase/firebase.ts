f36e114fa45f6b32bfddb338adc4f706d5c63ade
mbohorquez22 <miguel.lanzcom@gmail.com>
2023-01-23 15:20:14 -0500

    fix firebase

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -15,17 +15,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
-/* const firebaseConfig = {
-  apiKey: "AIzaSyA3HHPu8tepR32vPPN9MeTB-pHWVEjdNaE",
-  authDomain: "remo-584cc.firebaseapp.com",
-  databaseURL: "https://remo-584cc.firebaseio.com",
-  projectId: "remo-584cc",
-  storageBucket: "remo-584cc.appspot.com",
-  messagingSenderId: "15794658087",
-  appId: "1:15794658087:web:cd30f756acc93a4adc6702",
-  measurementId: "G-1VV34LT0FB"
-}; */
-
       const app = initializeApp(firebaseConfig);
       const database = getFirestore(app);
       const auth = getAuth(app)
